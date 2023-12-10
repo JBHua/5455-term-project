4dbce51eb50d8d9184239c5b425898ea797c9a5b
Yifan Huang <yifan.evan.huang@gmail.com>
2023-12-10 18:59:24 -0500

    update embedding test code

--- src/speaker_embedding_test.py
+++ src/speaker_embedding_test.py
@@ -11,15 +11,25 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 sub_collection = "client_id"
 # sub_collection = "collective"
 
-# gender = 'male'
-gender = 'female'
+gender = 'male'
+# gender = 'female'
 accent = 'us'
 
-t5_vanilla_model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts").to(device)
+model_name = ""
+
+def load_model():
+    if model_name == "":
+        return SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts").to(device)
+    else:
+        local_model_path = constants.MODEL_BASE_PATH + model_name
+        return SpeechT5ForTextToSpeech.from_pretrained(pretrained_model_name_or_path=local_model_path,
+                                                local_files_only=True).to(device)
+
 processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
 vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan").to(device)
 
-text = "the brown fox jumps over the lazy dog"
+# text = "the brown fox jumps over the lazy dog"
+text = "For the twentieth time that evening the two men shook hands"
 
 inputs = processor(text=text, return_tensors="pt").to(device)
 
@@ -27,7 +37,8 @@ embedding_file_path = constants.EMBEDDINGS_DIR_BASE_PATH + sub_collection + '/' 
 pre_trained_embeddings = torch.load(embedding_file_path)
 pre_trained_embeddings_tensor = torch.tensor(pre_trained_embeddings).unsqueeze(0).to(device)
 
-spectrogram = t5_vanilla_model.generate_speech(inputs["input_ids"], pre_trained_embeddings_tensor).to(device)
+loaded_model = load_model()
+spectrogram = loaded_model.generate_speech(inputs["input_ids"], pre_trained_embeddings_tensor).to(device)
 
 with torch.no_grad():
     speech = vocoder(spectrogram)
@@ -35,7 +46,7 @@ speech = speech.cpu()  # move back to CPU
 
 Audio(speech.numpy(), rate=16000)
 
-audio_file_name = f"{constants.AUDIO_OUTPUT_PATH + 't5_vanilla/' + sub_collection + '_' + accent + '_' + gender}.wav"
+audio_file_name = f"{constants.AUDIO_OUTPUT_PATH + 'test/' + sub_collection + '_' + accent + '_' + gender}.wav"
 
 sf.write(audio_file_name, speech.numpy(), samplerate=16000)
 
